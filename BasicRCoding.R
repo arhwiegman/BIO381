5f0b6c14e7193d645d62da60e60c3cd38bd1941e
Adrian Wiegman <32752714+arhwiegman@users.noreply.github.com>
2018-01-31 14:38:03 -0500

    update

---
+++ BasicRCoding.R
@@ -0,0 +1,20 @@
+# Basic R commands and usage
+# 2018 January 30
+# Adrian Wiegman 
+
+#Useful hotkey commands 
+#ctrl + shift + c puts # in first space of selected lines
+#ctrl + enter # ctrl enter
+#ctrl + shift + enter # runs the entire file
+
+#History shows recent console commants
+#Environment shows things contained in memory
+
+#using the assignment operator
+x<-5 #preferred
+print(x)
+y = 4 #legal but not used except in functions
+y = y + 1.1 #
+plantHeight <- 5.5
+
+
